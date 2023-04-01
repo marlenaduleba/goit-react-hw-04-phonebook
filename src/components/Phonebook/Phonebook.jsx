c06e5b4df4810b498519b115c9a9c7977a0730c8
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-01 16:28:29 +0200

    Section  made into hooks

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo } from 'react';
-import Section from 'components/Section/Section';
+import { Section } from 'components/Section/Section';
 import Filter from 'components/Filter/Filter';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
@@ -16,19 +16,16 @@ export const Phonebook = () => {
     const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
 
     if (parsedContacts) {
-     setContacts(parsedContacts);
-
+      setContacts(parsedContacts);
     } else {
       setContacts([
-          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ]);
-
-      
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]);
     }
-  }, []); 
+  }, []);
 
   useEffect(() => {
     if (!contacts.length) {
@@ -36,13 +33,9 @@ export const Phonebook = () => {
     }
 
     localStorage.setItem('contacts', JSON.stringify(contacts));
-
-   
   }, [contacts]);
 
   const onAddContact = contact => {
-    
-
     const searchUnique = contact.name.toLowerCase();
 
     if (contacts.find(({ name }) => name.toLowerCase() === searchUnique)) {
@@ -50,9 +43,7 @@ export const Phonebook = () => {
       return;
     }
 
- setContacts(state => [...state, contact]);
-
-
+    setContacts(state => [...state, contact]);
   };
 
   const handleFilter = e => {
@@ -63,35 +54,33 @@ export const Phonebook = () => {
     setContacts(state => state.filter(contact => contact.id !== id));
   };
 
- 
   const renderContacts = useMemo(() => {
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(filter.toLowerCase().trim())
     );
   }, [filter, contacts]);
 
-    return (
-      <div className={css.box}>
-        <Section title="Phonebook">
-          <ContactForm onAddContact={onAddContact} />
-        </Section>
+  return (
+    <div className={css.box}>
+      <Section title="Phonebook">
+        <ContactForm onAddContact={onAddContact} />
+      </Section>
 
-        <Section title="Contacts">
-          {contacts.length !== 0 ? (
-            <Filter handleFilter={handleFilter} />
-          ) : (
-            <p className={css.message}>
-              Looks like you don`t have any contacts yet or just clear them all.
-              Please add new contact.
-            </p>
-          )}
+      <Section title="Contacts">
+        {contacts.length !== 0 ? (
+          <Filter handleFilter={handleFilter} />
+        ) : (
+          <p className={css.message}>
+            Looks like you don`t have any contacts yet or just clear them all.
+            Please add new contact.
+          </p>
+        )}
 
-          <ContactList
-            contacts={renderContacts}
-            handleClickDelete={handleClickDelete}
-          />
-        </Section>
-      </div>
-    );
-  
-}
+        <ContactList
+          contacts={renderContacts}
+          handleClickDelete={handleClickDelete}
+        />
+      </Section>
+    </div>
+  );
+};
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './Section.module.css';
 
-export default class Section extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node,
-    ]).isRequired,
-  };
-  render() {
-    return (
-      <section>
-        <h2 className={css.title}>{this.props.title}</h2>
-        {this.props.children}
-      </section>
-    );
-  }
-}
+export const Section = ({ title, children }) => {
+  return (
+    <section>
+      <h2 className={css.title}>{title}</h2>
+      {children}
+    </section>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
